df3a0f83e826c8dffba492113d1a1dfdd04d7d33
magskwa <magali.egger@heig-vd.ch>
2022-10-18 16:55:47 +0200

    get the bucket

--- src/main/java/BucketManip.java
+++ src/main/java/BucketManip.java
@@ -1,2 +1,17 @@
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+
 public class BucketManip {
+    public static void main(String[] args){
+        BucketManip bm = new BucketManip();
+        Bucket bucket = bm.getBucket("amt.team11.diduno.education");
+        System.out.println(bucket.getName());
+    }
+
+    public Bucket getBucket(String bucketName){
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_WEST_2).build();
+        return s3.listBuckets().stream().filter(b -> b.getName().equals(bucketName)).findFirst().get();
+    }
 }
